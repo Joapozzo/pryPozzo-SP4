b05714a77e8d518b69a3412427e469ba2d3af116
Joaquin Pozzo <pozzojoa@gmail.com>
2022-11-16 07:21:40 -0300

    .

--- frmMain.cs
+++ frmMain.cs
@@ -12,7 +12,7 @@ namespace pryPozzo_SP4
 {
     public partial class Menu : Form
     {
-        
+        int e = 0;
         int i = 0;
         int f = 0;
         int c = 0;
@@ -51,67 +51,17 @@ namespace pryPozzo_SP4
 
         private void btnMozoDelDia_Click(object sender, EventArgs e)
         {
-            i = 0;
             int[] vecA = new int[4];
-            int Acumuladora = 0;
-            int varTotal = 0;
-            int varTotal1 = 0;
-            int varTotal2 = 0;
-            int varTotal3 = 0;
-            int varTotal4 = 0;
-            int[,] matA = new int[4, 4];
-            string valor;
-            for (int f = 0; f < 4; f++)
-            {
-                c = 0;
-                for (int c = 0; c < 4; c++)
-                {
-
-                    valor = dgvImportes.Rows[f].Cells[c + 1].Value.ToString();
-                    matA[f, c] = int.Parse(valor);
-                    Acumuladora = Acumuladora + matA[f, c];
-                    if (f == 0 && c == 4)
-                    {
-                        varTotal = Acumuladora;
-                        Acumuladora = 0;
-                    }
-                    if (f == 1 && c == 4)
-                    {
-                        varTotal1 = Acumuladora;
-                        Acumuladora = 0;
-                    }
-                    if (f == 2 && c == 4)
-                    {
-                        varTotal2 = Acumuladora;
-                        Acumuladora = 0;
-                    }
-                    if (f == 3 && c == 4)
-                    {
-                        varTotal3 = Acumuladora;
-                        Acumuladora = 0;
-                    }
-                    if (f == 4 && c == 4)
-                    {
-                        varTotal4 = Acumuladora;
-                        Acumuladora = 0;
-                    }
-                }
-            }
-            vecA[0] = varTotal;
-            vecA[1] = varTotal1;
-            vecA[2] = varTotal2;
-            vecA[3] = varTotal3;
-            vecA[4] = varTotal4;
+            i = 0;
             while (i <= 4)
             {
-                if (vecA[0] > vecA[1])
+                vecA[i] = dgvImportes.Rows[i].Cells[1].ColumnIndex + dgvImportes.Rows[i].Cells[2].ColumnIndex + dgvImportes.Rows[i].Cells[3].ColumnIndex + dgvImportes.Rows[i].Cells[4].ColumnIndex;
+                if (vecA[i] > vecA[i + 1])
                 {
-                    lblMozoDia.Text = "Julio";
+                    lblMozoDia.Text = "Total vendido del mozo:  " + vecA[i];
                 }
-                i = i + 1;
+                i++;
             }
-
-
         }
     }
 }
